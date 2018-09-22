8fd2e34122a9e7731a2173a76dd1133a2e232255
Jack Yang <jackjoeng@gmail.com>
2018-09-22 02:49:30 -0400

    heroku deploying errors

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let methodOverride = require("method-override");
-let PORT = process.env.PORT || 8080;
-let app = express();
 
-require('dotenv').config().
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method", ["POST", "GET"]));
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method", ["POST", "GET"]));
 
 let exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 let routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("App listening on http://localhost:" + PORT);
-  });
+  console.log("App listening on http://localhost:" + PORT);
+});
